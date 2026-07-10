621f07d1a8e00a373ecd38f91b7a9340b852edce
Ade Lee <25005892@myrp.edu.sg>
2026-07-10 15:47:49 +0800

    All the required project files and folder is here to start the CA2

---
+++ app.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const mysql = require('mysql2');
+const app = express();
+const multer = require('multer')
+
+
+const PORT = 3000;
+
+//Multer setup for file uploads : 
+const storage = multer.diskStorage({
+    destination: (req, file ,cb)=>{
+        cb(null, 'public/images');
+
+    },
+    filename: (req,file,cb)=>{
+        cb(null, file.originalname)
+    }
+});
+
+const upload = multer({storage:storage})
+ 
+// Create MySQL connection
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '', //Dont need to change
+    database: '' //Change based on database
+});
+ 
+connection.connect((err) => {
+    if (err) {
+        console.error('Error connecting to MySQL:', err);
+        return;
+    }
+    console.log('Connected to MySQL database');
+});
+ 
+// Set up view engine
+app.set('view engine', 'ejs');
+//  enable static files
+app.use(express.static('public'));
+
+
+//enable form processing
+app.use(express.urlencoded({
+    extended: false
+}))
+ 
+//Aden task 
+// app.get('/', (req, res) => {
+//     connection.query('SELECT * FROM TABLE', (error, results) => {
+//       if (error) throw error;
+//       res.render('index', { results }); // Render HTML page with data
+//     });
+// });
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
